8b6ccceef37a06f82ceeca984917283d655c6751
AswinTA <50104276+AswinTA@users.noreply.github.com>
2019-09-20 12:00:45 +0530

    Add files via upload

---
+++ a11yAiOverlay1.js
@@ -0,0 +1,7 @@
+console.log("hiiiiiiiiiiiii");
+var body = document.getElementsByTagName('body');
+body.onclick = function(){
+   console.log("clicked");
+}
+
+console.log("script injected successfullyy");
